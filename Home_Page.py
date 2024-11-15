a70370ce9f9bda79d55aab2737d6d52db6aaf9c5
Charlie-Rivers <charlieriv@icloud.com>
2024-11-15 15:30:52 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+st.header("CS 1301")
+st.subheader("Team 52, Web Development - Section C")
+st.subheader("Team Members: Charlie Rivers, Daniel Johns")
+
+# Introduction
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Charlie's Portfolio**: A comprehensive overview of Charlie's educational background, professional experiences, projects, skills, and activities.
+
+2. **Daniel's Portfolio**: A comprehensive overview of Charlie's educational background, professional experiences, projects, skills, and activities.
+
+3. **Section 1**: 
+
+4. **Section 2**: 
+
+""")
