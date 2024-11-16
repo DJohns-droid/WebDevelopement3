a37c725897c28d33776d17d9078f6b9547b5630c
Charlie-Rivers <charlieriv@icloud.com>
2024-11-16 18:20:38 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -3,10 +3,13 @@ import streamlit as st
 # Title of App
 st.title("Web Development Lab03")
 
-# Assignment Data 
+# Assignment Data
 st.header("CS 1301")
 st.subheader("Team 52, Web Development - Section C")
 st.subheader("Team Members: Charlie Rivers, Daniel Johns")
+
+# Image Upload
+uploaded_image = st.file_uploader("Upload a space-themed image", type=["png", "jpg", "jpeg"])
 
 # Introduction
 st.write("""
@@ -14,10 +17,9 @@ Welcome to our Streamlit Web Development Lab03 app! You can navigate between the
 
 1. **Charlie's Portfolio**: A comprehensive overview of Charlie's educational background, professional experiences, projects, skills, and activities.
 
-2. **Daniel's Portfolio**: A comprehensive overview of Charlie's educational background, professional experiences, projects, skills, and activities.
+2. **Daniel's Portfolio**: A comprehensive overview of Daniel's educational background, professional experiences, projects, skills, and activities.
 
-3. **Section 1**: 
+3. **Mars Dashboard**: Displays the latest weather data from Mars, powered by the NASA API.
 
-4. **Section 2**: 
-
+4. **NASA 2024 Pictures of the Day**: A gallery of stunning images from NASA's Astronomy Picture of the Day collection for 2024.
 """)
